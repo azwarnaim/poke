fb73b46083fc79de1802373b34d1dd9876838275
azwarnaim <azwardev@gmail.com>
2025-03-31 22:43:27 +0800

    RemoveUnuse

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,7 @@ import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import Slider from "@/components/slider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
